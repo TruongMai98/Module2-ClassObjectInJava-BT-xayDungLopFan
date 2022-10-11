4d231ce1637c8aa625bd03cf29ef5288986f0b47
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-10-11 15:10:24 +0700

    xay dung lop fan

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Fan fan1 = new Fan(3,true, 10, "yellow");
+        Fan fan2 = new Fan(2,true, 5, "red");
+
+        fan1.toString();
+        fan2.toString();
+    }
+}
